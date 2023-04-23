c2c8aa3c7687e23c65a89cc2292917d3d00a2b86
tanuafom <tanuafom@gmail.com>
2023-04-23 21:08:43 +1000

    day 2 commit

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -34,7 +34,7 @@ print("It's about to get intense, " + player_2_name + "! You will play with X.")
 
 def check_active_player():
    global turn_count
-   if turn_count / 2 == 0:
+   if turn_count / 2 != 0:
         return player_1_name
    else:
         return player_2_name
@@ -43,18 +43,45 @@ def check_active_player():
 def turn():
     global turn_count
     active_player = check_active_player()
-    if active_player == player_1_name:
-        print(player_1_name + ", it's your turn! Which cell do you want to mark as yours?")
-        print(board)
-        marked_cell = input()
-    elif active_player == player_2_name:
-        print(player_2_name + ", it's your turn! Which cell do you want to mark as yours?")
-        print(board)
-        marked_cell = input()
+    if turn_count < 5:
+        if active_player == player_1_name:
+            print(player_1_name + ", it's your turn! Which cell do you want to mark as yours?")
+            display_board()
+            marked_cell = input()
+            # return marked_cell_o
+        elif active_player == player_2_name:
+            print(player_2_name + ", it's your turn! Which cell do you want to mark as yours?")
+            display_board()
+            marked_cell = input()
+            # return marked_cell_x
+    elif turn_count >= 5:
+        check_win()
     else:
         print("Error")
     turn_count += 1
+    return marked_cell
+    return active_player
+
+def check_win(active_player):
+    if board["A"][0]==marked_cell and board["A"][1]== marked_cell and board["A"][2]==marked_cell:
+        winner(player_here)
+    elif board["B"][0]==marked_cell and board["B"][1]== marked_cell and board["B"][2]==marked_cell:
+        winner(player_here)
+    elif board["C"][0]==marked_cell and board["C"][1]== marked_cell and board["C"][2]==marked_cell:
+        winner(player_here)
+    elif board["A"][0]==marked_cell and board["B"][0]== marked_cell and board["C"][0]==marked_cell:
+        winner(player_here)
+    elif board["A"][1]==marked_cell and board["B"][1]== marked_cell and board["C"][1]==marked_cell:
+        winner(player_here)
+    elif board["A"][2]==marked_cell and board["B"][2]== marked_cell and board["C"][2]==marked_cell:
+        winner(player_here)
+    elif board["A"][0]==marked_cell and board["B"][1]== marked_cell and board["C"][2]==marked_cell:
+        winner(player_here)
+    elif board["A"][2]==marked_cell and board["B"][1]== marked_cell and board["C"][0]==marked_cell:
+        winner(player_here)
+    else:
+        display_board()
+        print("There has been a Tie")
 
 turn()
-
-# if turn_count >= 5:
+check_win()
