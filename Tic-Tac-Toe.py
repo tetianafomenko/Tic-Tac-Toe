ab8eb51e119517ab116b13b7c75878a908132ef2
tanuafom <tanuafom@gmail.com>
2023-04-24 22:18:23 +1000

    day 2 commit - need to fix replay flow

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,12 +1,3 @@
-# X Create a turns tracker
-
-# Create error handling if the input is not equal to o or O or x or X
-# Create win condition
-# After each turn, check for win condition
-# Write what happens when a win/draw condition is met
-
-turn_count = 1
-
 # Create the board 
 board = {
     "A": ["-","-","-"],
@@ -14,7 +5,7 @@ board = {
     "C": ["-","-","-"]}
 
 # Show the board in terminal
-def display_board():
+def display_board(board):
     print("~~~~~~~~~~~~~~~~~~~~")
     print("       BOARD")
     for cell in board:
@@ -22,66 +13,138 @@ def display_board():
     print("    0    1    2")
     print("~~~~~~~~~~~~~~~~~~~~")
 
-print("Player 1, what is your name?")
-player_1_name = input()
-print("Get ready to throw down," + player_1_name + "! You will play with O")
+# Assign names and marks to each player
+Player_1 = {'name': '', 'mark': 'O', 'winner': False}
+Player_2 = {'name': '', 'mark': 'X', 'winner': False}
 
-print("Player 2, what is your name?")
-player_2_name = input()
-print("It's about to get intense, " + player_2_name + "! You will play with X.")
+def reset_values():
+    Player_1 = {'name': '', 'mark': 'O', 'winner': False}
+    Player_2 = {'name': '', 'mark': 'X', 'winner': False}
+    board = {
+    "A": ["-","-","-"],
+    "B": ["-","-","-"],
+    "C": ["-","-","-"]}
 
-# print(board)
-
-def check_active_player():
-   global turn_count
-   if turn_count / 2 != 0:
-        return player_1_name
-   else:
-        return player_2_name
-
-# Offer a turn to each player
-def turn():
-    global turn_count
-    active_player = check_active_player()
-    if turn_count < 5:
-        if active_player == player_1_name:
-            print(player_1_name + ", it's your turn! Which cell do you want to mark as yours?")
-            display_board()
-            marked_cell = input()
-            # return marked_cell_o
-        elif active_player == player_2_name:
-            print(player_2_name + ", it's your turn! Which cell do you want to mark as yours?")
-            display_board()
-            marked_cell = input()
-            # return marked_cell_x
-    elif turn_count >= 5:
-        check_win()
-    else:
-        print("Error")
-    turn_count += 1
-    return marked_cell
+# Get the names and communicate the marks
+def intro():
+    print("Player 1, what is your name?")
+    Player_1["name"] = input()
+    Player_1["mark"] = str(Player_1['name'][0])
+    print("Get ready to throw down, " + Player_1["name"] + "! You will play with " + Player_1["mark"] + "!")
+    print("Player 2, what is your name?")
+    Player_2["name"] = input()
+    Player_2["mark"] = str(Player_2['name'][0])
+    print("Get ready to throw down, " + Player_2["name"] + "! You will play with " + Player_2["mark"] + "!")
+    active_player = Player_1
     return active_player
 
-def check_win(active_player):
-    if board["A"][0]==marked_cell and board["A"][1]== marked_cell and board["A"][2]==marked_cell:
-        winner(player_here)
-    elif board["B"][0]==marked_cell and board["B"][1]== marked_cell and board["B"][2]==marked_cell:
-        winner(player_here)
-    elif board["C"][0]==marked_cell and board["C"][1]== marked_cell and board["C"][2]==marked_cell:
-        winner(player_here)
-    elif board["A"][0]==marked_cell and board["B"][0]== marked_cell and board["C"][0]==marked_cell:
-        winner(player_here)
-    elif board["A"][1]==marked_cell and board["B"][1]== marked_cell and board["C"][1]==marked_cell:
-        winner(player_here)
-    elif board["A"][2]==marked_cell and board["B"][2]== marked_cell and board["C"][2]==marked_cell:
-        winner(player_here)
-    elif board["A"][0]==marked_cell and board["B"][1]== marked_cell and board["C"][2]==marked_cell:
-        winner(player_here)
-    elif board["A"][2]==marked_cell and board["B"][1]== marked_cell and board["C"][0]==marked_cell:
-        winner(player_here)
+def announce_winner(playa):
+    print("Congratulations, " + playa['name'] + ", you have won!!!")
+    display_board(board)
+    return playa
+    
+def check_win(symbol, active_player, turn):
+    if board["A"][0] == active_player['mark'] and board["A"][1] == active_player['mark'] and board["A"][2] == active_player['mark']:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["B"][0] == symbol and board["B"][1] == symbol and board["B"][2] == symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["C"][0] == symbol and board["C"][1] == symbol and board["C"][2] == symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["A"][0] == symbol and board["B"][0] == symbol and board["C"][0] == symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["A"][1] == symbol and board["B"][1] == symbol and board["C"][1] == symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["A"][2] == symbol and board["B"][2] == symbol and board["C"][2] == symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["A"][0]==symbol and board["B"][1]== symbol and board["C"][2]==symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
+    elif board["A"][2]==symbol and board["B"][1]== symbol and board["C"][0]==symbol:
+        active_player['winner'] = True
+        return announce_winner(active_player)
+        reset_values()
     else:
-        display_board()
-        print("There has been a Tie")
+        if turn == 9:
+            print("There has been a Tie. Do you want to play again? Y/N")
+            answer = input()
+            if answer == "Y" or answer == "y":
+                game()
+            elif answer == "N" or answer == "n":
+                print("Farewell!")
 
-turn()
-check_win()
+# Offer a turn to each player
+def take_turns(active_player=Player_1):
+    for turn in range(10):
+        if Player_1['winner'] == True or Player_2['winner'] == True:
+            print("Do you want to play again? Y/N")
+            answer = input()
+            if answer == "Y" or answer == "y":
+                game()
+            elif answer == "N" or answer == "n":
+                print("Farewell!")
+        else:
+            display_board(board)
+            print('Turn for ' + active_player["name"] + '. Move on which space?')
+            the_move = input()
+            insert(the_move, active_player, turn)
+            # check_win(the_move, active_player, turn)
+            turn += 1
+            if active_player == Player_1:
+                active_player = Player_2
+            else:
+                active_player = Player_1
+
+# Update the table with entered coordinates
+def insert(coord, active_player, turn):
+    try:
+      x, y = coord[0].upper(), int(coord[1])
+      if active_player == Player_1:
+        if board[x][y] == Player_1['mark'] or board[x][y] == Player_2['mark']:
+            print("That space is already occupied, try a new space!")
+            new_coord = input()
+            insert(new_coord, active_player, turn)
+        else:
+            board[x][y] = Player_1["mark"]
+            check_win(Player_1["mark"], Player_1, turn)
+      elif active_player == Player_2:
+        if board[x][y] == Player_1['mark'] or board[x][y] == Player_2["mark"]:
+            print('That space is already occupied, try a new space!')
+            new_coord = input()
+            insert(new_coord, active_player, turn)
+        else:
+          board[x][y] = Player_2["mark"]
+          check_win(Player_2["mark"], Player_2, turn)
+    except ValueError: #if input is a special character 
+        print("That was an invalid value. Wanna try again?")
+        new_coord = input()
+        insert(new_coord, active_player, turn)
+    except IndexError: #if input is out of range
+        print("That was an invalid entry. Wanna try again?")
+        new_coord = input()
+        insert(new_coord, active_player, turn)
+    except:
+        print("That was a weird entry, wanna try that again?")
+        new_coord = input()
+        insert(new_coord, active_player, turn)
+    return coord, active_player, turn
+
+# Make the layout of the game
+def game():
+    intro()
+    take_turns()
+
+# Play game
+game()
